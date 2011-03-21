88ca3daaf30392bca4a7a9ad4e0baf83c218840b
Alex Norman <x37v.alex@gmail.com>
2011-03-20 19:42:14 -0700

    initial sync works

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "midi_usb.h"
+#include <avr/io.h>
 
 //this example simply echos midi back, for now
 
@@ -12,8 +13,28 @@ int main(void) {
    midi_usb_init(&usb_midi);
    midi_register_fallthrough_callback(&usb_midi, fallthrough_callback);
 
+   //left adjust, use AVCC as ref
+   ADMUX = _BV(ADLAR) | _BV(REFS0);
+   ADCSRA = _BV(ADEN);
+   ADCSRA |= _BV(ADSC);
+   ADCSRA |= _BV(ADPS2) | _BV(ADPS1);
+   uint16_t filtered = 0;
+   uint8_t last = 0;
    while(1){
       midi_device_process(&usb_midi);
+      //new adc val
+      if (ADCSRA & _BV(ADIF)) {
+         uint8_t val = ADCH;
+         filtered = (filtered + val) >> 1;
+         val = filtered >> 1;
+         if (last != val) {
+            last = val;
+            midi_send_cc(&usb_midi, 0, 0, val);
+         }
+         //start a new conversion
+         ADCSRA |= _BV(ADIF);
+         ADCSRA |= _BV(ADSC);
+      }
    }
 
    return 0;
